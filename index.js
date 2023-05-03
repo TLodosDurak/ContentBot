e7bccf965af37c351adc7d4b49e0573cebd821ca
TLodosDurak <tlodosdurak@gmail.com>
2023-05-03 16:41:58 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,64 @@
+// Import required libraries
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+// Set up Express app and enable CORS
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.post('/create-video', async (req, res) => {
+  try {
+    const topic = req.body.topic;
+    
+    // Call OpenAI API
+    const openaiResponse = await callOpenAI(topic);
+
+    // Parse the top 10 list
+    const top10List = parseTop10List(openaiResponse);
+
+    // Find images for each item in the top 10 list
+    const imageUrls = await Promise.all(top10List.map(item => findImage(item)));
+
+    // Find a suitable music video
+    const musicVideoUrl = await findMusicVideo(topic);
+
+    // Create the video using FFmpeg.js, fabric.js, and RecordRTC (to be implemented on the frontend)
+    // ...
+
+    // Return URLs of the images and music video
+    res.json({
+      top10List,
+      imageUrls,
+      musicVideoUrl
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred while processing your request.' });
+  }
+});
+
+// Helper functions
+async function callOpenAI(topic) {
+  // ...
+}
+
+function parseTop10List(openaiResponse) {
+  // ...
+}
+
+async function findImage(query) {
+  // ...
+}
+
+async function findMusicVideo(query) {
+  // ...
+}
+
+// Start server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
